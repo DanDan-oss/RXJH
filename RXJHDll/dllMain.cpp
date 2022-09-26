7e64e3045567fb78ac16cae830fadeaa000662f3
MOHUI <1187480520@qq.com>
2022-09-26 22:31:05 +0800

    cccc Signed-off-by: HUIMO <1187480520@qq.com>

--- RXJHDll/dllMain.cpp
+++ RXJHDll/dllMain.cpp
@@ -9,6 +9,8 @@ HMODULE hMod = NULL;
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
+
+	// asdsadasdasdsa
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
